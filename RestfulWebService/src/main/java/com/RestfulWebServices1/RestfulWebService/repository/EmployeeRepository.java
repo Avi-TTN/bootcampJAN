b4c7055ac62aa399b1e8baa46a06afdddd11bedf
Avi-TTN <avi.vyas@tothenew.com>
2025-03-06 13:01:43 +0530

    Delete RestfulWebService directory

--- RestfulWebService/src/main/java/com/RestfulWebServices1/RestfulWebService/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.RestfulWebServices1.RestfulWebService.repository;
-
-import com.RestfulWebServices1.RestfulWebService.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
